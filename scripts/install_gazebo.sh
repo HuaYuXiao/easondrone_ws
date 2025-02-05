dbb14dd6c809c29e3e86b17a39a8736e11d76e96
easonhua <14250267+hyx020222@user.noreply.gitee.com>
2025-02-05 02:27:24 +0000

    add scripts install_gazebo.sh
    Signed-off-by: easonhua <14250267+hyx020222@user.noreply.gitee.com>

---
+++ scripts/install_gazebo.sh
@@ -0,0 +1,5 @@
+sudo apt install ros-noetic-moveit-msgs ros-noetic-object-recognition-msgs ros-noetic-octomap-msgs ros-noetic-camera-info-manager  ros-noetic-control-toolbox ros-noetic-polled-camera ros-noetic-controller-manager ros-noetic-transmission-interface ros-noetic-joint-limits-interface
+
+sudo apt install gazebo11 libgazebo11-dev
+
+sudo apt install ros-noetic-gazebo-ros
