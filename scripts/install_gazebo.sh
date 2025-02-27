b27664558cb4e158daf359d1a4b857a6ac766723
easonhua <14250267+hyx020222@user.noreply.gitee.com>
2025-02-27 02:16:07 +0000

    update scripts/install_gazebo.sh.
    Signed-off-by: easonhua <14250267+hyx020222@user.noreply.gitee.com>

--- scripts/install_gazebo.sh
+++ scripts/install_gazebo.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+# Exit Anaconda environment
+conda deactivate
+conda deactivate
+
+sudo apt update
+
 sudo apt install ros-noetic-moveit-msgs ros-noetic-object-recognition-msgs ros-noetic-octomap-msgs ros-noetic-camera-info-manager  ros-noetic-control-toolbox ros-noetic-polled-camera ros-noetic-controller-manager ros-noetic-transmission-interface ros-noetic-joint-limits-interface
-
 sudo apt install gazebo11 libgazebo11-dev
-
 sudo apt install ros-noetic-gazebo-ros
