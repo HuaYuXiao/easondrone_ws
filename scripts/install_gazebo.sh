b2ad75823413f41fb7d7bdc212aaff589671e718
easonhua <12010508@mail.sustech.edu.cn>
2025-05-18 19:04:51 +0800

    apt install -y

--- scripts/install_gazebo.sh
+++ scripts/install_gazebo.sh
@@ -5,6 +5,6 @@ conda deactivate
 
 sudo apt update
 
-sudo apt install ros-noetic-moveit-msgs ros-noetic-object-recognition-msgs ros-noetic-octomap-msgs ros-noetic-camera-info-manager  ros-noetic-control-toolbox ros-noetic-polled-camera ros-noetic-controller-manager ros-noetic-transmission-interface ros-noetic-joint-limits-interface
-sudo apt install gazebo11 libgazebo11-dev
-sudo apt install ros-noetic-gazebo-ros
+sudo apt install -y ros-noetic-moveit-msgs ros-noetic-object-recognition-msgs ros-noetic-octomap-msgs ros-noetic-camera-info-manager  ros-noetic-control-toolbox ros-noetic-polled-camera ros-noetic-controller-manager ros-noetic-transmission-interface ros-noetic-joint-limits-interface
+sudo apt install -y gazebo11 libgazebo11-dev
+sudo apt install -y ros-noetic-gazebo-ros
